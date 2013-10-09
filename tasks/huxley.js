1132a78c2a600ad178932ec297d52f94ae5aba50
Cheng Lou <chenglou92@gmail.com>
2013-10-09 15:17:00 -0400

    init attempt at running huxley: fails

--- tasks/huxley.js
+++ tasks/huxley.js
@@ -8,43 +8,26 @@
 
 'use strict';
 
+var huxley = require('huxley');
+
 module.exports = function(grunt) {
-
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
   grunt.registerMultiTask('huxley', 'Grunt task for node-huxley.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    var browser = this.options.browser;
+    var path;
+    if (this.files[0] && this.files[0].src) {
+      path = this.files[0].src;
+    }
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+    switch (this.options.action) {
+      case 'record':
+        huxley.recordTasks(browser);
+        return true;
+      case 'update':
+        huxley.playbackTasksAndSaveScreenshots(browser, path);
+        return true;
+      default:
+        huxley.playbackTasksAndCompareScrenshots(browser, path);
+        return true;
+    }
   });
-
 };
